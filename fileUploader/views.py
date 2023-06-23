830b5df71f5440735528d1c7cfeb5bf3bdafb5e5
Githaiga <pythongeeko@gmail.com>
2023-06-23 12:32:16 +0300

    update qrCodes media endpoint

--- fileUploader/views.py
+++ fileUploader/views.py
@@ -18,7 +18,19 @@ class QrCodeUploadView(APIView):
             return Response({'file_url': file_url}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    # def put(self, request, file_name, format=None):
+    #     serializer = QrCodeFileSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         file_path = serializer.save()  # Save the file and get the file path
+    #         new_file_path = f"{settings.QRCODE_MEDIA_ROOT}/{file_name}"  # Path to the new file you want to replace with
+    #         with open(new_file_path, 'rb') as new_file:
+    #             # Update the existing file with the new file data
+    #             serializer.update(serializer.instance, {'file': new_file})
+    #         file_url = request.build_absolute_uri(settings.MEDIA_URL + file_path)
+    #         return Response({'file_url': file_url}, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SecureRepoFileUploadView(APIView):
