858725f883e2445576cbd8d4105f09ea246b460b
Githaiga <pythongeeko@gmail.com>
2023-06-28 10:23:15 +0300

    update media endpoint

--- dowellFileUploader/settings.py
+++ dowellFileUploader/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 QRCODE_MEDIA_ROOT = os.path.join(BASE_DIR, 'qrCodes')
 SECURE_REP0_MEDIA_ROOT = os.path.join(BASE_DIR, 'secureRepo')
--- dowellFileUploader/urls.py
+++ dowellFileUploader/urls.py
@@ -9,14 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('uploadfiles/', include('fileUploader.urls')),
     path('api/v2/', include('app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += [
-    path('media/qrCodes/<path:path>', serve, {'document_root': settings.QRCODE_MEDIA_ROOT}),
-    path('media/secureRepo/<path:path>', serve, {'document_root': settings.SECURE_REP0_MEDIA_ROOT}),
-    path('media/announcements/<path:path>', serve, {'document_root': settings.ANNOUNCEMENT_MEDIA_ROOT}),
-    path('media/Image/<path:path>', serve, {'document_root': settings.CAM_COMPONENT_IMAGES_MEDIA_ROOT}),
-    path('media/Video/<path:path>', serve, {'document_root': settings.CAM_COMPONENT_VIDEOS_MEDIA_ROOT}),
-
-    
+    path('qrCodes/<path:path>', serve, {'document_root': settings.QRCODE_MEDIA_ROOT}),
+    path('secureRepo/<path:path>', serve, {'document_root': settings.SECURE_REP0_MEDIA_ROOT}),
+    path('announcements/<path:path>', serve, {'document_root': settings.ANNOUNCEMENT_MEDIA_ROOT}),
+    path('camera_component_images/<path:path>', serve, {'document_root': settings.CAM_COMPONENT_IMAGES_MEDIA_ROOT}),
+    path('camera_component_videos/<path:path>', serve, {'document_root': settings.CAM_COMPONENT_VIDEOS_MEDIA_ROOT}),
 ]
