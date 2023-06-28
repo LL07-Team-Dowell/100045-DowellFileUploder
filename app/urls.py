8c2bb159bb2bca86643aea84b84166f2d041fe12
Githaiga <pythongeeko@gmail.com>
2023-06-28 15:20:03 +0300

    camera video and image upload

--- app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('upload-image/', ImageUploadView.as_view(), name='image-upload'),
-    path('upload-video/', VideoUploadView.as_view(), name='video-upload'),
-]
