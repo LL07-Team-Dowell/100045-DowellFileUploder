708b58c1909b798148a771d9e9ba6a944d1e296a
Manish <mdashsharma95@gmail.com>
2024-04-03 16:41:12 +0530

    healthcheck endpoint

--- dowellFileUploader/urls.py
+++ dowellFileUploader/urls.py
@@ -3,9 +3,10 @@ from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-
+from healthcheck import *
 
 urlpatterns = [
+    path('', health_check.as_view()),
     path('admin/', admin.site.urls),
     path('uploadfiles/', include('fileUploader.urls')),
 ]
